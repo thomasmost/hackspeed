952a5eb9a1a9899dcb78c581828de2f8b17eac6b
tcm_NIMBUS <tomconmo@gmail.com>
2019-02-23 15:40:23 -0800

    authentication works on front-end

--- web/app/modules/auth/auth0_lock.ts
+++ web/app/modules/auth/auth0_lock.ts
@@ -2,14 +2,18 @@ import Auth0Lock from "auth0-lock";
 
 export const lock = new Auth0Lock(
    "x2pKjxcL9XLMqWO1k5H6Tr3eFcU06Ggr",
-   "hackspeed-dev.auth0.com",
-   {
-      auth: {
-         redirectUrl: "localhost:3000/callback",
-         responseType: "code",
-         // params: {
-         //    scope: "openid email" // Learn about scopes: https://auth0.com/docs/scopes
-         // }
-      }
-   }
+   "hackspeed-dev.auth0.com"
 );
+
+// Listen for authenticated event; pass the result to a function as authResult
+lock.on("authenticated", function(authResult) {
+   // Call getUserInfo using the token from authResult
+   lock.getUserInfo(authResult.accessToken, function(error, profile) {
+     if (error) {
+       // Handle error
+       return;
+     }
+     // Store the token from authResult for later use
+     localStorage.setItem("accessToken", authResult.accessToken);
+   });
+ });
