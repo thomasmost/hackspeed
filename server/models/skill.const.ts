e5ee3a492a9b2b4718ebf3707a7e01d75a9b3804
chrisgarber <christophermgarber@gmail.com>
2019-02-23 23:43:37 -0800

    why this doesn't work no one knoooooooow :ghost::moon::+1:

---
+++ server/models/skill.const.ts
@@ -0,0 +1,33 @@
+export const DevelopmentSkills = [
+   "React",
+   "AngularJs",
+   "Angular 2+",
+   "C#",
+   "Node",
+   "Bootstrapping",
+   "iOS",
+   "Android",
+   "Python",
+   "Machine Learning",
+   "Data Engineering",
+   "Javascript"
+];
+
+export const DesignSkills = [
+   "Visual",
+   "Computer Interfaces",
+   "Application",
+   "Web Design"
+];
+
+export const ProductSkills = [
+   "People Management",
+   "Product Definition",
+   "Triage"
+];
+
+export const Skills = {
+   "Development": DevelopmentSkills,
+   "Design": DesignSkills,
+   "Product": ProductSkills
+};
