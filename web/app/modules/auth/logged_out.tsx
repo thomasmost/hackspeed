6d9429a8b3e6d353387848e38fe3e4274b10e0e5
tcm_NIMBUS <tomconmo@gmail.com>
2019-02-23 21:45:11 -0800

    styled logout page

--- web/app/modules/auth/logged_out.tsx
+++ web/app/modules/auth/logged_out.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { clientSession } from "./auth.session";
+import { Link } from "react-router-dom";
+import { container } from "./logged_out.scss";
 
 
 export class LoggedOutPage extends React.Component {
@@ -10,8 +12,15 @@ export class LoggedOutPage extends React.Component {
 
    render () {
       return (
-         <div>
-            May the Hacks be Ever in your Favor.
+         <div className={container}>
+            <div>
+               May the Hacks be Ever in your Favor.
+            </div>
+            <Link to="/login" >
+               <button>
+                  Log Back In
+               </button>
+            </Link>
          </div>
       );
    }
